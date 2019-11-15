85523b3066ee76497036ea261a4e10dde4016977
Albert Luganga <ahosea15@gmail.com>
2019-11-15 18:43:40 +0300

    added images

---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { DirectoryMenu } from '../../components/directory-menu/directory-menu.component'
+import './home.styles.scss'
+
+const Home = () => (
+    <div className='home'>
+        <DirectoryMenu />
+    </div>
+)
+
+export default Home
