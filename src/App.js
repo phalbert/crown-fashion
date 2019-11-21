37b5651e2af28758efaabbf1327650d1c5942005
Albert Luganga <ahosea15@gmail.com>
2019-11-21 14:12:27 +0300

    redirect to home if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import './App.css';
@@ -24,8 +24,8 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot(snapshot => {
           setCurrentUser({
-              id: snapshot.id,
-              ...snapshot.data()
+            id: snapshot.id,
+            ...snapshot.data()
           })
         })
       }
@@ -45,15 +45,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route path='/shop' component={Shop} />
-          <Route path='/auth' component={Auth} />
+          <Route exact path='/auth' render={() => this.props.currentUser ?
+            <Redirect to='/' /> :
+            <Auth />
+          } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
