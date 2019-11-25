03246a875e32c2614d2663e0c96ae9f08f83aee6
Albert Luganga <ahosea15@gmail.com>
2019-11-25 22:02:01 +0300

    Created cart item component, connected it to dropdown; add cart count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,15 +5,19 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, cartCount }) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className='item-count'>0</span>
+        <span className='item-count'>{cartCount}</span>
     </div>
 )
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartCount: cartItems.length
+})
 
 const mapDipatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null, mapDipatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDipatchToProps)(CartIcon)
