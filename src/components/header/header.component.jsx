b0f8ae88f6cd379d928a4cea749316979d5dc9de
Albert Luganga <ahosea15@gmail.com>
2019-12-02 03:11:15 +0300

    refactored collection item to use css in js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
@@ -10,10 +9,7 @@ import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
-
-import './header.styles.scss'
-
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv } from './header.styles'
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles'
 
 const Header = ({ currentUser, hidden }) => {
     return (
@@ -26,7 +22,7 @@ const Header = ({ currentUser, hidden }) => {
                 <OptionLink to="/contact">CONTACT</OptionLink>
                 {
                     currentUser ?
-                        <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+                        <OptionLink as='div' to='' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
                         :
                         <OptionLink to="/auth">SIGN IN</OptionLink>
                 }
